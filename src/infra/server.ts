6d300e5f5cddb1e52b1571d3552ecee109b49432
leo-henrique <leonardo0507.business@gmail.com>
2024-10-25 13:54:03 -0300

    fix: change what is executed when starting the api

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,15 +1,10 @@
-import { sql } from "drizzle-orm";
-import { drizzle } from "./database";
 import { env } from "./env";
 import { app } from "./http/app";
 
 (async () => {
   await app.ready();
   await app.listen({ host: "0.0.0.0", port: env.API_PORT });
-  await drizzle.execute(sql`SELECT 'status';`);
 
   console.log(`Application "${env.API_NAME}" is running!`);
-
-  if (env.NODE_ENV === "development")
-    console.log(`http://localhost:${env.API_PORT}/docs`);
+  console.log(`http://${env.API_HOST}:${env.API_PORT}/docs`);
 })();
